8c86376d5a93f6dee72fdadd4907ddd7b6e1a7c2
MongXin <1393907820@qq.com>
2025-04-04 17:52:41 +0800

    finished lab01

--- week01/examples/lab/main.cpp
+++ week01/examples/lab/main.cpp
@@ -4,12 +4,12 @@
 using namespace std;
 int main()
 {
-    int num1 =　2147483647;
-    int num2 =　1;
-    int result =　0;
+    int num1 = 2147483647;
+    int num2 = 1;
+    int result = 0;
 
-    result = add(num1, num2)；
+    result = add(num1, num2);
 
-    cout << “The result is ” << result << endl;
+    cout << "The result is " << result << endl;
     return 0;
 }
